69212c9d23da08588ccfce47d9d826e3d6aed753
EmeraldAmbre <ambresihdom@outlook.fr>
2024-06-20 14:44:36 +0200

    Update at 14.15 - Bonus Mechanic
    Added & fixed : bonus mechanic (prefab & script)

--- Assets/Script/Bonus/BonusController.cs
+++ Assets/Script/Bonus/BonusController.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
-public class BonusControler : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class BonusControler : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private int m_bonusPoints = 10;
+
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        
+        if (collider.gameObject.layer == 7) {
+
+            GameController._playerScore += m_bonusPoints;
+            BackGroundRandomiser._nbBonusOnScreen -= 1;
+
+            GetComponent<CircleCollider2D>().enabled = false;
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<Light2D>().enabled = false;
+
+            Destroy(gameObject, 10f);
+        }
     }
 }
