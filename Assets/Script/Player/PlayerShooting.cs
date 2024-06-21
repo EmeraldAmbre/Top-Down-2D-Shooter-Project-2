7f88f4d8c20a3b4cb2336fdcc982712c762767b8
EmeraldAmbre <ambresihdom@outlook.fr>
2024-06-21 09:53:51 +0200

    Update PlayerShooting.cs

--- Assets/Script/Player/PlayerShooting.cs
+++ Assets/Script/Player/PlayerShooting.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class PlayerShooting : MonoBehaviour {
 
     [SerializeField] private GameObject m_missilePrefab;
-    AudioSource m_audioSource;
     [SerializeField] AudioClip[] m_laserSounds;
-    int m_laserIndex;
 
+    AudioSource m_audioSource;
+    int m_laserIndex;
 
     void Update() {
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire1")) {
+
             Quaternion fireRotation = transform.rotation;
             Vector3 firePosition = transform.position;
             GameObject laser = Instantiate(m_missilePrefab, firePosition, fireRotation);
-            if (m_laserSounds.Length != 0)
-            {
+
+            if (m_laserSounds.Length != 0) {
                 m_laserIndex = Random.Range(0, m_laserSounds.Length);
                 laser.GetComponent<AudioSource>().PlayOneShot(m_laserSounds[m_laserIndex]);
             }
